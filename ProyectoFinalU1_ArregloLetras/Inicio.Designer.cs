48a9f7cf593aa1de9b14d4a0572ba6ee67bbe064
DESKTOP-QKJ565Q\Nestor Puentes <nestorpuentesin@gmail.com>
2020-10-22 02:21:56 -0500

    PROYECTO TERMINADO. Programa Corregido y Diseño completado.

--- ProyectoFinalU1_ArregloLetras/Inicio.Designer.cs
+++ ProyectoFinalU1_ArregloLetras/Inicio.Designer.cs
@@ -28,51 +28,151 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Inicio));
             this.Btn_Iniciar = new System.Windows.Forms.Button();
             this.Btn_Creditos = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // Btn_Iniciar
             // 
-            this.Btn_Iniciar.Location = new System.Drawing.Point(73, 200);
+            this.Btn_Iniciar.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Btn_Iniciar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Btn_Iniciar.Font = new System.Drawing.Font("Arial Nova", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Iniciar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Btn_Iniciar.Location = new System.Drawing.Point(79, 173);
             this.Btn_Iniciar.Name = "Btn_Iniciar";
             this.Btn_Iniciar.Size = new System.Drawing.Size(188, 43);
             this.Btn_Iniciar.TabIndex = 0;
             this.Btn_Iniciar.Text = "Iniciar";
-            this.Btn_Iniciar.UseVisualStyleBackColor = true;
+            this.Btn_Iniciar.UseVisualStyleBackColor = false;
             this.Btn_Iniciar.Click += new System.EventHandler(this.Btn_Iniciar_Click);
             // 
             // Btn_Creditos
             // 
-            this.Btn_Creditos.Location = new System.Drawing.Point(73, 279);
+            this.Btn_Creditos.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Btn_Creditos.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Btn_Creditos.Font = new System.Drawing.Font("Arial Nova", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Creditos.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Btn_Creditos.Location = new System.Drawing.Point(79, 256);
             this.Btn_Creditos.Name = "Btn_Creditos";
             this.Btn_Creditos.Size = new System.Drawing.Size(188, 43);
             this.Btn_Creditos.TabIndex = 1;
-            this.Btn_Creditos.Text = "Créditos";
-            this.Btn_Creditos.UseVisualStyleBackColor = true;
+            this.Btn_Creditos.Text = "Créditos (↓)";
+            this.Btn_Creditos.UseVisualStyleBackColor = false;
             this.Btn_Creditos.Click += new System.EventHandler(this.Btn_Creditos_Click);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(73, 31);
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(188, 133);
+            this.pictureBox1.Size = new System.Drawing.Size(310, 168);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Tahoma", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.groupBox1.Location = new System.Drawing.Point(13, 325);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(309, 224);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos del equipo #4";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(6, 199);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(299, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "UNIVERSIDAD TECNOLOFICA DE TORREON - 4B TIDSM";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(11, 153);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(275, 14);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "•\t19170068 – Néstor Josué Puentes Inchaurregui";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(11, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(247, 14);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "•\t19170087 – Marcos Amaury Rodriguez Ruiz";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(11, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(224, 14);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "•\t19170106 – José Felipe Muñiz Fonseca";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 14);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "INTEGRANTES";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(292, 14);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "•\t19170038 – Raúl Alejandro de los Santos Anguiano";
             // 
             // Inicio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(334, 361);
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(334, 561);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Btn_Iniciar);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.Btn_Creditos);
-            this.Controls.Add(this.Btn_Iniciar);
             this.Name = "Inicio";
-            this.Text = "Form1";
+            this.Text = "Arreglo de Letras";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -82,6 +182,13 @@
         private System.Windows.Forms.Button Btn_Iniciar;
         private System.Windows.Forms.Button Btn_Creditos;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- ProyectoFinalU1_ArregloLetras/MenuPrincipal.Designer.cs
+++ ProyectoFinalU1_ArregloLetras/MenuPrincipal.Designer.cs
@@ -28,151 +28,215 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.Btn_Buscar = new System.Windows.Forms.Button();
             this.Btn_Insertar = new System.Windows.Forms.Button();
-            this.Btn_Modificar = new System.Windows.Forms.Button();
             this.Btn_Eliminar = new System.Windows.Forms.Button();
             this.Btn_Salir = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.txBuscador = new System.Windows.Forms.TextBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Btn_Modificar = new System.Windows.Forms.Button();
             this.txReemplazar = new System.Windows.Forms.TextBox();
+            this.txModificar = new System.Windows.Forms.TextBox();
             this.listBox2 = new System.Windows.Forms.ListBox();
             this.Btn_Limpiar = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Tt_Texto = new System.Windows.Forms.ToolTip(this.components);
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // Btn_Buscar
             // 
-            this.Btn_Buscar.Location = new System.Drawing.Point(703, 38);
+            this.Btn_Buscar.BackColor = System.Drawing.Color.Khaki;
+            this.Btn_Buscar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Btn_Buscar.Font = new System.Drawing.Font("Arial Nova", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Buscar.Location = new System.Drawing.Point(83, 22);
             this.Btn_Buscar.Name = "Btn_Buscar";
-            this.Btn_Buscar.Size = new System.Drawing.Size(58, 33);
+            this.Btn_Buscar.Size = new System.Drawing.Size(81, 33);
             this.Btn_Buscar.TabIndex = 1;
             this.Btn_Buscar.Text = "Buscar";
-            this.Btn_Buscar.UseVisualStyleBackColor = true;
+            this.Btn_Buscar.UseVisualStyleBackColor = false;
             this.Btn_Buscar.Click += new System.EventHandler(this.Btn_Buscar_Click);
             // 
             // Btn_Insertar
             // 
-            this.Btn_Insertar.Location = new System.Drawing.Point(12, 80);
+            this.Btn_Insertar.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.Btn_Insertar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Btn_Insertar.Font = new System.Drawing.Font("Arial Nova", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Insertar.Location = new System.Drawing.Point(12, 49);
             this.Btn_Insertar.Name = "Btn_Insertar";
             this.Btn_Insertar.Size = new System.Drawing.Size(139, 45);
             this.Btn_Insertar.TabIndex = 2;
             this.Btn_Insertar.Text = "Insertar";
-            this.Btn_Insertar.UseVisualStyleBackColor = true;
+            this.Btn_Insertar.UseVisualStyleBackColor = false;
             this.Btn_Insertar.Click += new System.EventHandler(this.Btn_Insertar_Click);
-            // 
-            // Btn_Modificar
-            // 
-            this.Btn_Modificar.Location = new System.Drawing.Point(195, 80);
-            this.Btn_Modificar.Name = "Btn_Modificar";
-            this.Btn_Modificar.Size = new System.Drawing.Size(139, 45);
-            this.Btn_Modificar.TabIndex = 3;
-            this.Btn_Modificar.Text = "Modificar";
-            this.Btn_Modificar.UseVisualStyleBackColor = true;
-            this.Btn_Modificar.Click += new System.EventHandler(this.Btn_Modificar_Click);
             // 
             // Btn_Eliminar
             // 
-            this.Btn_Eliminar.Location = new System.Drawing.Point(370, 80);
+            this.Btn_Eliminar.BackColor = System.Drawing.Color.LightCoral;
+            this.Btn_Eliminar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Btn_Eliminar.Font = new System.Drawing.Font("Arial Nova", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Eliminar.Location = new System.Drawing.Point(171, 49);
             this.Btn_Eliminar.Name = "Btn_Eliminar";
-            this.Btn_Eliminar.Size = new System.Drawing.Size(139, 45);
+            this.Btn_Eliminar.Size = new System.Drawing.Size(152, 45);
             this.Btn_Eliminar.TabIndex = 4;
             this.Btn_Eliminar.Text = "Eliminar";
-            this.Btn_Eliminar.UseVisualStyleBackColor = true;
+            this.Btn_Eliminar.UseVisualStyleBackColor = false;
             this.Btn_Eliminar.Click += new System.EventHandler(this.Btn_Eliminar_Click);
             // 
             // Btn_Salir
             // 
-            this.Btn_Salir.Location = new System.Drawing.Point(615, 286);
+            this.Btn_Salir.BackColor = System.Drawing.Color.DimGray;
+            this.Btn_Salir.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Btn_Salir.Font = new System.Drawing.Font("Arial Nova", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Salir.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Btn_Salir.Location = new System.Drawing.Point(345, 263);
             this.Btn_Salir.Name = "Btn_Salir";
-            this.Btn_Salir.Size = new System.Drawing.Size(139, 45);
+            this.Btn_Salir.Size = new System.Drawing.Size(180, 45);
             this.Btn_Salir.TabIndex = 5;
             this.Btn_Salir.Text = "Salir";
-            this.Btn_Salir.UseVisualStyleBackColor = true;
+            this.Btn_Salir.UseVisualStyleBackColor = false;
             this.Btn_Salir.Click += new System.EventHandler(this.Btn_Salir_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(12, 45);
+            this.textBox1.Font = new System.Drawing.Font("Arial Nova", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 14);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(497, 29);
+            this.textBox1.Size = new System.Drawing.Size(311, 27);
             this.textBox1.TabIndex = 6;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
             // txBuscador
             // 
-            this.txBuscador.Location = new System.Drawing.Point(615, 45);
+            this.txBuscador.Font = new System.Drawing.Font("Arial Nova", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txBuscador.Location = new System.Drawing.Point(25, 26);
             this.txBuscador.Name = "txBuscador";
-            this.txBuscador.Size = new System.Drawing.Size(82, 20);
+            this.txBuscador.Size = new System.Drawing.Size(52, 27);
             this.txBuscador.TabIndex = 7;
+            this.txBuscador.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txBuscador_KeyPress);
             // 
             // listBox1
             // 
+            this.listBox1.Font = new System.Drawing.Font("Arial Nova", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(12, 133);
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(12, 102);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(139, 199);
+            this.listBox1.Size = new System.Drawing.Size(139, 204);
             this.listBox1.TabIndex = 8;
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.Btn_Modificar);
             this.groupBox1.Controls.Add(this.txReemplazar);
-            this.groupBox1.Location = new System.Drawing.Point(195, 133);
+            this.groupBox1.Controls.Add(this.txModificar);
+            this.groupBox1.Font = new System.Drawing.Font("Arial Nova", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(171, 118);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(139, 64);
+            this.groupBox1.Size = new System.Drawing.Size(152, 121);
             this.groupBox1.TabIndex = 9;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Reemplazar por";
             // 
+            // Btn_Modificar
+            // 
+            this.Btn_Modificar.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.Btn_Modificar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Btn_Modificar.Font = new System.Drawing.Font("Arial Nova", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Modificar.Location = new System.Drawing.Point(7, 69);
+            this.Btn_Modificar.Name = "Btn_Modificar";
+            this.Btn_Modificar.Size = new System.Drawing.Size(139, 45);
+            this.Btn_Modificar.TabIndex = 11;
+            this.Btn_Modificar.Text = "Modificar";
+            this.Btn_Modificar.UseVisualStyleBackColor = false;
+            this.Btn_Modificar.Click += new System.EventHandler(this.Btn_Modificar_Click);
+            // 
             // txReemplazar
             // 
-            this.txReemplazar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txReemplazar.Location = new System.Drawing.Point(21, 19);
+            this.txReemplazar.Font = new System.Drawing.Font("Arial Nova", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txReemplazar.Location = new System.Drawing.Point(78, 34);
             this.txReemplazar.Name = "txReemplazar";
-            this.txReemplazar.Size = new System.Drawing.Size(94, 29);
-            this.txReemplazar.TabIndex = 7;
+            this.txReemplazar.Size = new System.Drawing.Size(60, 27);
+            this.txReemplazar.TabIndex = 10;
+            this.txReemplazar.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txReemplazar_KeyPress);
+            // 
+            // txModificar
+            // 
+            this.txModificar.Font = new System.Drawing.Font("Arial Nova", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txModificar.Location = new System.Drawing.Point(13, 34);
+            this.txModificar.Name = "txModificar";
+            this.txModificar.Size = new System.Drawing.Size(60, 27);
+            this.txModificar.TabIndex = 9;
             // 
             // listBox2
             // 
+            this.listBox2.Font = new System.Drawing.Font("Arial Nova", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBox2.FormattingEnabled = true;
-            this.listBox2.Location = new System.Drawing.Point(615, 80);
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(25, 61);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(139, 199);
+            this.listBox2.Size = new System.Drawing.Size(139, 164);
             this.listBox2.TabIndex = 10;
             // 
             // Btn_Limpiar
             // 
-            this.Btn_Limpiar.Location = new System.Drawing.Point(195, 286);
+            this.Btn_Limpiar.BackColor = System.Drawing.Color.Maroon;
+            this.Btn_Limpiar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Btn_Limpiar.Font = new System.Drawing.Font("Arial Nova", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Limpiar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Btn_Limpiar.Location = new System.Drawing.Point(171, 261);
             this.Btn_Limpiar.Name = "Btn_Limpiar";
-            this.Btn_Limpiar.Size = new System.Drawing.Size(139, 45);
+            this.Btn_Limpiar.Size = new System.Drawing.Size(152, 45);
             this.Btn_Limpiar.TabIndex = 11;
             this.Btn_Limpiar.Text = "Limpiar TODO";
-            this.Btn_Limpiar.UseVisualStyleBackColor = true;
+            this.Btn_Limpiar.UseVisualStyleBackColor = false;
             this.Btn_Limpiar.Click += new System.EventHandler(this.Btn_Limpiar_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txBuscador);
+            this.groupBox2.Controls.Add(this.Btn_Buscar);
+            this.groupBox2.Controls.Add(this.listBox2);
+            this.groupBox2.Font = new System.Drawing.Font("Arial Nova", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(345, 14);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(180, 243);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Buscador";
+            // 
+            // Tt_Texto
+            // 
+            this.Tt_Texto.AutoPopDelay = 10000;
+            this.Tt_Texto.InitialDelay = 200;
+            this.Tt_Texto.IsBalloon = true;
+            this.Tt_Texto.ReshowDelay = 0;
             // 
             // MenuPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(766, 343);
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(541, 320);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.Btn_Limpiar);
-            this.Controls.Add(this.listBox2);
-            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.txBuscador);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.Btn_Salir);
             this.Controls.Add(this.Btn_Eliminar);
-            this.Controls.Add(this.Btn_Modificar);
             this.Controls.Add(this.Btn_Insertar);
-            this.Controls.Add(this.Btn_Buscar);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "MenuPrincipal";
             this.Text = "MenuPrincipal";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -181,15 +245,18 @@
         #endregion
         private System.Windows.Forms.Button Btn_Buscar;
         private System.Windows.Forms.Button Btn_Insertar;
-        private System.Windows.Forms.Button Btn_Modificar;
         private System.Windows.Forms.Button Btn_Eliminar;
         private System.Windows.Forms.Button Btn_Salir;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox txBuscador;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.TextBox txReemplazar;
         private System.Windows.Forms.ListBox listBox2;
         private System.Windows.Forms.Button Btn_Limpiar;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ToolTip Tt_Texto;
+        private System.Windows.Forms.Button Btn_Modificar;
+        private System.Windows.Forms.TextBox txReemplazar;
+        private System.Windows.Forms.TextBox txModificar;
     }
 }
